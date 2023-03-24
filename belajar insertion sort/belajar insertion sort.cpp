083e59ea1f80b2538b1d08a4333572e203f62cda
agilimam <agilimamalhafizi04@gmail.com>
2023-03-24 15:30:36 +0700

    menambahkan array {j +1)

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -36,4 +36,12 @@ void insertionsort() {
 	for (int i = 1; i < n; i++) {
 
 		temp = arr[i];
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
 
