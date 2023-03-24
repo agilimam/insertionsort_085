87489bc71f36b43a7737f53b05f16c41adc6d854
agilimam <agilimamalhafizi04@gmail.com>
2023-03-24 15:24:20 +0700

    input jumlah data array

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -4,3 +4,15 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "masukkan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+
